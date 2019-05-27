a6ec8fd2e83452ce3e06b037edb4412fed2c0fe8
sapphi-red <green@sapphi.red>
2019-05-27 18:06:16 +0900

    Update: apiの自動生成

---
+++ scripts/generate-api.js
@@ -0,0 +1,27 @@
+const request = require("request-promise-native");
+const fs = require("fs").promises;
+const { promisify } = require("util");
+const { exec } = require("child_process");
+
+const URL =
+  "https://raw.githubusercontent.com/sapphi-red/webengineer_naro-_7_server/master/swagger.yaml";
+
+const execAsync = promisify(exec);
+
+(async () => {
+  const res = await request(URL);
+
+  await fs.mkdir("./scripts/bin/");
+  await fs.writeFile("./scripts/bin/swagger.yaml", res, "utf-8");
+
+  const { stdout, stderr } = await execAsync(
+    "npx openapi-generator generate -g typescript-axios -i ./scripts/bin/swagger.yaml -o ./src/api"
+  );
+
+  if (stderr) {
+    console.error(stderr);
+  }
+  if (stdout) {
+    console.log(stdout);
+  }
+})();
