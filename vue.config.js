7086928806629c2a823b3976b16b4e10a4998e64
sapphi-red <green@sapphi.red>
2019-05-27 19:59:04 +0900

    Fix: CORS

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,3 @@
 module.exports = {
-  lintOnSave: process.env.NODE_ENV !== "production",
-  devServer: {
-    proxy: {
-      "^/api": {
-        target: "http://150.95.157.67:12100",
-        pathRewrite: {
-          "^/api/": "/"
-        }
-      }
-    },
-    overlay: {
-      warnings: true,
-      errors: true
-    }
-  }
+  lintOnSave: process.env.NODE_ENV !== "production"
 };
