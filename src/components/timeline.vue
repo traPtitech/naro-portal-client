1308799c127de51defb387b5a127253967bdf127
mazrean <shunsuke.w04@gmail.com>
2019-07-22 00:50:38 +0900

    全てのスペースのみのtweetを不可にする

--- src/components/timeline.vue
+++ src/components/timeline.vue
@@ -194,9 +194,7 @@ export default {
       upDateTweetTimer: null,
       tweetRule: [
         v => !!v || '空のTweetはできません',
-        v =>
-          !!v.replace(' ', '').replace('　', '') ||
-          'スペースのみのTweetはできません',
+        v => !!v.replace(/\s+/g, '') || 'スペースのみのTweetはできません',
         v => v.length <= 200 || 'Tweetの字数はは200文字までです',
       ],
     }
