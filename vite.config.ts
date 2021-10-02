9444a29ce47668d8984140320b97d6caf11ddcda
logica <84759675+logica0419@users.noreply.github.com>
2021-10-03 06:59:27 +0900

    proxy設定を修正

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 9001,
     proxy: {
       "/api": {
-        target: "http://localhost/9000",
+        target: "http://localhost:9000",
         changeOrigin: true,
       },
     },
